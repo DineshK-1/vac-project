27856843e4210a4d9f1f1521289029985bfab42e
Dinesh <dineshkumar.software@gmail.com>
2022-01-05 08:27:11 +0530

    Model with filefield

--- Frontend/models.py
+++ Frontend/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
+
+
 class FrontendModels(models.Model):
+    def ReturnType(self, filename):
+        return f'{self.Type}/{filename}'
     Category_List = [
-        ("1", "HTML"),
-        ("2", "CSS"),
-        ("3", "JS"),
+        ("HTML", "HTML"),
+        ("CSS", "CSS"),
+        ("JS", "JS"),
     ]
 
-    Type = models.CharField(max_length=1, choices=Category_List)
+    Type = models.CharField(max_length=4, choices=Category_List)
     Title = models.CharField(max_length = 200, primary_key = True)
-    Description = models.FileField()
+    Description = models.FileField(upload_to = ReturnType)
--- Frontend/views.py
+++ Frontend/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import FrontendModels
+import requests
 
-def Frontend(request):
-    return render(request, "Frontend.html")
+def Frontend(request, _Type, _Title):
+    Data = FrontendModels.objects.get(Type = _Type, Title = _Title)
+    r = requests.get( "http://127.0.0.1:8000/uploads/" + str(Data.Description), allow_redirects=False)
+    PageContent = r.content
+    return render(request, "Frontend.html", {"ModelData": FrontendModels.objects.all, "PageData":Data, "PageContent": PageContent})
